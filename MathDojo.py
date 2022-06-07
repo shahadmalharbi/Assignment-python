5d1006cc255b949e3f036c04fe45d3839d614ad2
Shahad Alharbi <103144756+shahadmalharbi@users.noreply.github.com>
2022-06-07 14:11:06 +0300

    Add files via upload

---
+++ MathDojo.py
@@ -0,0 +1,30 @@
+class MathDojo:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, num, *nums):
+        self.result += num
+        for n in nums:
+            self.result += n
+        return self
+
+    def subtract(self, num, *nums):
+        self.result -= num
+        for n in nums:
+            self.result -= n
+        return self
+
+
+md = MathDojo()
+x = md.add(2).add(2,5,1).subtract(3,2).result
+print(x)
+
+md1 = MathDojo()
+y = md1.add(3).add(10,5,1).subtract(3,3).result
+print(y)
+
+md2 = MathDojo()
+y = md2.add(3).add(10,5,1,6).subtract(3).result
+print(y)
+
+
